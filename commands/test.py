77ec1d691d14c97ce16fac16e60d78363c85ff1d
stefanitos <65655225+stefanitos@users.noreply.github.com>
2024-02-06 11:17:14 +0100

    Revert

--- commands/test.py
+++ commands/test.py
@@ -2,19 +2,17 @@
 from AnitakuWrapper import AnitakuWrapper
 from discord.commands import ApplicationContext
 from discord.ext import commands
-from .database_cog import DatabaseCog
 
 from main import TEST_GUILDS
 
 
-class Test(DatabaseCog):
+class Test(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.slash_command(name="test", aliases=["t"], description="Test command", guild_ids=TEST_GUILDS)
     async def test(self, ctx: ApplicationContext):
         await ctx.respond("Test command")
-        await super().add_anime("Test", "https://www.example.com")
 
 
 def setup(bot):
